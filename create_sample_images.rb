f4461540e418b5a9fcf4ae82b8fb1b4769bae7bd
Tomo Krajina <tkrajina@gmail.com>
2013-10-06 17:40:22 +0200

    Create sample images

--- create_sample_images.rb
+++ create_sample_images.rb
@@ -1,26 +1,28 @@
 require_relative 'images'
 
-=begin
-istra = Elevations::elevation_image(400, 400, [45, 46], [13, 14], 300)
-istra.display
-=end
+def save_image(image, file_name)
+    image.write(file_name)
+    puts "Saves #{file_name}"
+end
 
-=begin
+image = Elevations::elevation_image(400, 400, [45, 46], [13, 14], 300)
+save_image(image, 'istra.png')
+#istra.display
+
 miami = [25.787676, -80.224145]
 image = Elevations::elevation_image(400, 400, [miami[0] - 1, miami[0] + 1.5], [miami[1] - 2, miami[1] + 0.5], 40)
-image.save('miami.png')
-=end
+save_image(image, 'miami.png')
+#image.display
 
-=begin
 rio_de_janeiro = [-22.908333, -43.196389]
 image = Elevations::elevation_image(400, 400, [rio_de_janeiro[0] - 0.5, rio_de_janeiro[0] + 2], [rio_de_janeiro[1] - 0.5, rio_de_janeiro[1] + 2], 1000)
-image.save('rio.png')
-=end
+save_image(image, 'rio.png')
+#image.display
 
 sidney = [-33.859972, 151.211111]
 image = Elevations::elevation_image(400, 400, [sidney[0] - 1.5, sidney[0] + 1], [sidney[1] - 1.5, sidney[1] + 1], 1000)
-image.save('sidney.png')
+save_image(image, 'sidney.png')
+#image.display
 
-=begin
-Elevations::world_undulation_image(600, 300).display
-=end
+image = Elevations::world_undulation_image(600, 300)
+save_image(image, 'undulations.png')
